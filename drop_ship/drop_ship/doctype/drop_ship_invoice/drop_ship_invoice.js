eee13a9d88eb53c5925e4a3be1d2450257a1e4be
Revant Nandgaonkar <revant.one@gmail.com>
2016-01-31 14:29:24 +0530

    Basic Functionality

--- drop_ship/drop_ship/doctype/drop_ship_invoice/drop_ship_invoice.js
+++ drop_ship/drop_ship/doctype/drop_ship_invoice/drop_ship_invoice.js
@@ -1,450 +1,91 @@
-// Copyright (c) 2015, Revant Nandgaonkar and Contributors
-// License: GNU General Public License v3. See license.txt
+// see license.txt
 
-// print heading
-cur_frm.pformat.print_heading = 'Invoice';
+var receivable_account = "";
+var income_account = "";
+var pr = 0.0;
 
-{% include 'selling/sales_common.js' %};
-
-frappe.provide("erpnext.accounts");
-erpnext.accounts.DropShipInvoiceController = erpnext.selling.SellingController.extend({
-	onload: function() {
-		var me = this;
-		this._super();
-
-		if(!this.frm.doc.__islocal && !this.frm.doc.customer && this.frm.doc.debit_to) {
-			// show debit_to in print format
-			this.frm.set_df_property("debit_to", "print_hide", 0);
-		}
-
-		// toggle to pos view if is_pos is 1 in user_defaults
-		if ((is_null(this.frm.doc.is_pos) && cint(frappe.defaults.get_user_default("is_pos"))===1) || this.frm.doc.is_pos) {
-			if(this.frm.doc.__islocal && !this.frm.doc.amended_from && !this.frm.doc.customer) {
-				this.frm.set_value("is_pos", 1);
-				this.is_pos(function() {
-					if (cint(frappe.defaults.get_user_defaults("fs_pos_view"))===1)
-						erpnext.pos.toggle(me.frm, true);
-				});
-			}
-		}
-
-		erpnext.queries.setup_queries(this.frm, "Warehouse", function() {
-			return erpnext.queries.warehouse(me.frm.doc);
-		});
-	},
-
-	refresh: function(doc, dt, dn) {
-		this._super();
-
-		if(cur_frm.msgbox && cur_frm.msgbox.$wrapper.is(":visible")) {
-			// hide new msgbox
-			cur_frm.msgbox.hide();
-		}
-
-		cur_frm.dashboard.reset();
-
-		this.frm.toggle_reqd("due_date", !this.frm.doc.is_return);
-
-		this.show_general_ledger();
-
-		if(doc.update_stock) this.show_stock_ledger();
-
-		if(doc.docstatus==1 && !doc.is_return) {
-
-			var is_delivered_by_supplier = false;
-
-			is_delivered_by_supplier = cur_frm.doc.items.some(function(item){
-				return item.is_delivered_by_supplier ? true : false;
-			})
-
-			cur_frm.add_custom_button(doc.update_stock ? __('Sales Return') : __('Credit Note'),
-				this.make_sales_return, __("Make"));
-			cur_frm.page.set_inner_btn_group_as_primary(__("Make"));
-
-			if(cint(doc.update_stock)!=1) {
-				// show Make Delivery Note button only if Sales Invoice is not created from Delivery Note
-				var from_delivery_note = false;
-				from_delivery_note = cur_frm.doc.items
-					.some(function(item) {
-						return item.delivery_note ? true : false;
-					});
-
-				if(!from_delivery_note && !is_delivered_by_supplier) {
-					cur_frm.add_custom_button(__('Delivery'), cur_frm.cscript['Make Delivery Note'], 
-						__("Make"));
-				}
-			}
-
-			if(doc.outstanding_amount!=0 && !cint(doc.is_return)) {
-				cur_frm.add_custom_button(__('Payment Request'), this.make_payment_request, __("Make"));
-				cur_frm.add_custom_button(__('Payment'), cur_frm.cscript.make_bank_entry, __("Make"));
-			}
-
-		}
-
-		// Show buttons only when pos view is active
-		if (cint(doc.docstatus==0) && cur_frm.page.current_view_name!=="pos" && !doc.is_return) {
-			cur_frm.cscript.sales_order_btn();
-		}
-
-		this.set_default_print_format();
-	},
-
-	set_default_print_format: function() {
-		// set default print format to POS type
-		if(cur_frm.doc.is_pos) {
-			if(cur_frm.pos_print_format) {
-				cur_frm.meta._default_print_format = cur_frm.meta.default_print_format;
-				cur_frm.meta.default_print_format = cur_frm.pos_print_format;
-			}
-		} else {
-			if(cur_frm.meta._default_print_format) {
-				cur_frm.meta.default_print_format = cur_frm.meta._default_print_format;
-				cur_frm.meta._default_print_format = null;
-			}
-		}
-	},
-
-	sales_order_btn: function() {
-		this.$sales_order_btn = cur_frm.add_custom_button(__('Sales Order'),
-			function() {
-				frappe.model.map_current_doc({
-					method: "drop_ship.drop_ship.doctype.drop_ship_invoice.drop_ship_invoice.make_drop_ship_invoice",
-					source_doctype: "Sales Order",
-					get_query_filters: {
-						docstatus: 1,
-						status: ["not in", ["Stopped", "Closed"]],
-						per_billed: ["<", 99.99],
-						customer: cur_frm.doc.customer || undefined,
-						company: cur_frm.doc.company
-					}
-				})
-			}, __("Get items from"));
-	},
-
-	tc_name: function() {
-		this.get_terms();
-	},
-
-	is_pos: function(doc, dt, dn, callback_fn) {
-		cur_frm.cscript.hide_fields(this.frm.doc);
-		if(cur_frm.doc.__missing_values_set) return;
-		if(cint(this.frm.doc.is_pos)) {
-			if(!this.frm.doc.company) {
-				this.frm.set_value("is_pos", 0);
-				msgprint(__("Please specify Company to proceed"));
-			} else {
-				var me = this;
-				return this.frm.call({
-					doc: me.frm.doc,
-					method: "set_missing_values",
-					callback: function(r) {
-						if(!r.exc) {
-							if(r.message && r.message.print_format) {
-								cur_frm.pos_print_format = r.message.print_format;
-							}
-							cur_frm.doc.__missing_values_set = true;
-							me.frm.script_manager.trigger("update_stock");
-							frappe.model.set_default_values(me.frm.doc);
-							me.set_dynamic_labels();
-							me.calculate_taxes_and_totals();
-							if(callback_fn) callback_fn();
-							frappe.after_ajax(function() {
-								cur_frm.doc.__missing_values_set = false;
-							})
-						}
-					}
-				});
-			}
-		}
-	},
-
-	customer: function() {
-		var me = this;
-		if(this.frm.updating_party_details) return;
-
-		erpnext.utils.get_party_details(this.frm,
-			"erpnext.accounts.party.get_party_details", {
-				posting_date: this.frm.doc.posting_date,
-				party: this.frm.doc.customer,
-				party_type: "Customer",
-				account: this.frm.doc.debit_to,
-				price_list: this.frm.doc.selling_price_list,
-			}, function() {
-			me.apply_pricing_rule();
-		})
-	},
-
-	debit_to: function() {
-		var me = this;
-		if(this.frm.doc.debit_to) {
-			me.frm.call({
-				method: "frappe.client.get_value",
-				args: {
-					doctype: "Account",
-					fieldname: "account_currency",
-					filters: { name: me.frm.doc.debit_to },
-				},
-				callback: function(r, rt) {
-					if(r.message) {
-						me.frm.set_value("party_account_currency", r.message.account_currency);
-						me.set_dynamic_labels();
-					}
-				}
-			});
-		}
-	},
-
-	allocated_amount: function() {
-		this.calculate_total_advance();
-		this.frm.refresh_fields();
-	},
-
-	write_off_outstanding_amount_automatically: function() {
-		if(cint(this.frm.doc.write_off_outstanding_amount_automatically)) {
-			frappe.model.round_floats_in(this.frm.doc, ["grand_total", "paid_amount"]);
-			// this will make outstanding amount 0
-			this.frm.set_value("write_off_amount",
-				flt(this.frm.doc.grand_total - this.frm.doc.paid_amount - this.frm.doc.total_advance, precision("write_off_amount"))
-			);
-			this.frm.toggle_enable("write_off_amount", false);
-
-		} else {
-			this.frm.toggle_enable("write_off_amount", true);
-		}
-
-		this.calculate_outstanding_amount(false);
-		this.frm.refresh_fields();
-	},
-
-	write_off_amount: function() {
-		this.set_in_company_currency(this.frm.doc, ["write_off_amount"]);
-		this.write_off_outstanding_amount_automatically();
-	},
-
-	paid_amount: function() {
-		this.set_in_company_currency(this.frm.doc, ["paid_amount"]);
-		this.write_off_outstanding_amount_automatically();
-	},
-
-	items_add: function(doc, cdt, cdn) {
-		var row = frappe.get_doc(cdt, cdn);
-		this.frm.script_manager.copy_from_first_row("items", row, ["income_account", "cost_center"]);
-	},
-
-	set_dynamic_labels: function() {
-		this._super();
-		this.hide_fields(this.frm.doc);
-	},
-
-	items_on_form_rendered: function() {
-		erpnext.setup_serial_no();
-	},
-
-	make_sales_return: function() {
-		frappe.model.open_mapped_doc({
-			method: "erpnext.accounts.doctype.sales_invoice.sales_invoice.make_sales_return",
-			frm: cur_frm
-		})
+calculate_totals = function(doc) {
+	var items = doc.items || [];
+	doc.purchase_total = 0.0;
+	doc.total = 0.0;
+	for(var i=0;i<items.length;i++) {
+		//if (!items[i].purchase_rate || items[i].purchase_rate == 0.0){
+		frappe.call({
+        	method: "frappe.client.get",
+        	args: {
+        		doctype: "Item Price",
+        		filters: {
+        			"price_list": doc.price_list,
+        			"item_code": items[i].item_code
+        		}
+        	},
+        	callback: function (data) {
+				pr = data.message.price_list_rate;
+  	    	}
+    	});
+			// frappe.call({
+	  //       	method: "drop_ship.drop_ship.doctype.drop_ship_invoice.drop_ship_invoice.get_item_prices",
+	  //       	args: {
+	  //       		price_list: doc.price_list,
+	  //       		item_code: items[i].item_code
+	  //       	},
+	  //       	callback: function (data) {
+			// 		items[i].purchase_rate = data.message.item_price;
+	  //       	}
+	  //   	});
+		//}
+		items[i].purchase_rate = flt(pr);
+		items[i].amount = flt(flt(items[i].rate) * flt(items[i].qty));
+		doc.total += items[i].amount;
+		items[i].purchase_amount = flt(flt(items[i].purchase_rate) * flt(items[i].qty));
+		//frappe.model.set_value(frm.doctype, frm.docname, "purchase_total", items[i].purchase_amount);
+		doc.purchase_total += items[i].purchase_amount;
 	}
-});
+	doc.total_commission = doc.total - doc.purchase_total;
+	doc.commission_rate = ((doc.total - doc.purchase_total) / doc.total) * 100;
+	refresh_field('purchase_rate');
+	refresh_field('purchase_total');
+	refresh_field('total');
+	refresh_field('purchase_amount');
+}
 
-// for backward compatibility: combine new and previous states
-$.extend(cur_frm.cscript, new erpnext.accounts.DropShipInvoiceController({frm: cur_frm}));
-
-// Hide Fields
-// ------------
-cur_frm.cscript.hide_fields = function(doc) {
-	par_flds = ['project_name', 'due_date', 'is_opening', 'source', 'total_advance', 'get_advances_received',
-		'advances', 'sales_partner', 'commission_rate', 'total_commission', 'advances', 'from_date', 'to_date'];
-
-	if(cint(doc.is_pos) == 1) {
-		hide_field(par_flds);
-	} else {
-		for (i in par_flds) {
-			var docfield = frappe.meta.docfield_map[doc.doctype][par_flds[i]];
-			if(!docfield.hidden) unhide_field(par_flds[i]);
+get_items_from_so = function() {
+	frappe.model.map_current_doc({
+		method: "drop_ship.drop_ship.doctype.drop_ship_invoice.drop_ship_invoice.make_drop_ship_invoice",
+		source: cur_frm.doc.name,
+		get_query_filters: {
+			docstatus: 1,
+			status: ["not in", ["Stopped", "Closed"]],
+			per_billed: ["<", 99.99],
+			customer: cur_frm.doc.customer || undefined,
+			company: cur_frm.doc.company
 		}
-	}
-
-	item_flds_stock = ['serial_no', 'batch_no', 'actual_qty', 'expense_account', 'warehouse', 'expense_account', 'warehouse']
-	cur_frm.fields_dict['items'].grid.set_column_disp(item_flds_stock,
-		(cint(doc.update_stock)==1 ? true : false));
-
-	// India related fields
-	if (frappe.boot.sysdefaults.country == 'India') unhide_field(['c_form_applicable', 'c_form_no']);
-	else hide_field(['c_form_applicable', 'c_form_no']);
-
-	this.frm.toggle_enable("write_off_amount", !!!cint(doc.write_off_outstanding_amount_automatically));
-
-	cur_frm.refresh_fields();
-}
-
-
-cur_frm.cscript.mode_of_payment = function(doc) {
-	if(doc.is_pos) {
-		return cur_frm.call({
-			method: "erpnext.accounts.doctype.sales_invoice.sales_invoice.get_bank_cash_account",
-			args: {
-				"mode_of_payment": doc.mode_of_payment,
-				"company": doc.company
-			 },
-			 callback: function(r, rt) {
-				if(r.message) {
-					cur_frm.set_value("cash_bank_account", r.message["account"]);
-				}
-
-			 }
-		});
-	 }
-}
-
-cur_frm.cscript.update_stock = function(doc, dt, dn) {
-	cur_frm.cscript.hide_fields(doc, dt, dn);
-}
-
-cur_frm.cscript['Make Delivery Note'] = function() {
-	frappe.model.open_mapped_doc({
-		method: "erpnext.accounts.doctype.sales_invoice.sales_invoice.make_delivery_note",
-		frm: cur_frm
 	})
 }
 
-cur_frm.cscript.make_bank_entry = function() {
-	return frappe.call({
-		method: "erpnext.accounts.doctype.journal_entry.journal_entry.get_payment_entry_against_invoice",
-		args: {
-			"dt": "Sales Invoice",
-			"dn": cur_frm.doc.name
-		},
-		callback: function(r) {
-			var doclist = frappe.model.sync(r.message);
-			frappe.set_route("Form", doclist[0].doctype, doclist[0].name);
-		}
-	});
+cur_frm.cscript.refresh = function(doc, dt, dn) {
+	calculate_totals(doc);
 }
 
-cur_frm.fields_dict.cash_bank_account.get_query = function(doc) {
-	return {
-		filters: [
-			["Account", "account_type", "in", ["Cash", "Bank"]],
-			["Account", "root_type", "=", "Asset"],
-			["Account", "is_group", "=",0],
-			["Account", "company", "=", doc.company]
-		]
-	}
-}
-
-cur_frm.fields_dict.write_off_account.get_query = function(doc) {
-	return{
-		filters:{
-			'report_type': 'Profit and Loss',
-			'is_group': 0,
-			'company': doc.company
-		}
-	}
-}
-
-// Write off cost center
-//-----------------------
-cur_frm.fields_dict.write_off_cost_center.get_query = function(doc) {
-	return{
-		filters:{
-			'is_group': 0,
-			'company': doc.company
-		}
-	}
-}
-
-//project name
-//--------------------------
-cur_frm.fields_dict['project_name'].get_query = function(doc, cdt, cdn) {
-	return{
-		query: "erpnext.controllers.queries.get_project_name",
-		filters: {'customer': doc.customer}
-	}
-}
-
-// Income Account in Details Table
-// --------------------------------
-cur_frm.set_query("income_account", "items", function(doc) {
-	return{
-		query: "erpnext.controllers.queries.get_income_account",
-		filters: {'company': doc.company}
+frappe.ui.form.on("Drop Ship Invoice", {
+	onload: function(frm) {
+		frappe.call({
+        	method: "drop_ship.drop_ship.doctype.drop_ship_settings.drop_ship_settings.get_account",
+        	args: {
+        		company: frm.doc.company
+        	},
+        	callback: function (data) {
+        		receivable_account = data.message[0].account;
+				income_account = data.message[1].account;
+        		if(!receivable_account){
+	        		msgprint (__("Set Receivable account in Drop Ship Settings"));
+        		}
+        		if(!income_account){
+	        		msgprint (__("Set Income account in Drop Ship Settings"));
+        		}
+        	}
+    	});
 	}
 });
 
-// expense account
-if (sys_defaults.auto_accounting_for_stock) {
-	cur_frm.fields_dict['items'].grid.get_field('expense_account').get_query = function(doc) {
-		return {
-			filters: {
-				'report_type': 'Profit and Loss',
-				'company': doc.company,
-				"is_group": 0
-			}
-		}
-	}
-}
-
-
-// Cost Center in Details Table
-// -----------------------------
-cur_frm.fields_dict["items"].grid.get_field("cost_center").get_query = function(doc) {
-	return {
-		filters: {
-			'company': doc.company,
-			"is_group": 0
-		}
-	}
-}
-
-cur_frm.cscript.income_account = function(doc, cdt, cdn) {
-	erpnext.utils.copy_value_in_all_row(doc, cdt, cdn, "items", "income_account");
-}
-
-cur_frm.cscript.expense_account = function(doc, cdt, cdn) {
-	erpnext.utils.copy_value_in_all_row(doc, cdt, cdn, "items", "expense_account");
-}
-
-cur_frm.cscript.cost_center = function(doc, cdt, cdn) {
-	erpnext.utils.copy_value_in_all_row(doc, cdt, cdn, "items", "cost_center");
-}
-
-cur_frm.cscript.on_submit = function(doc, cdt, cdn) {
-	$.each(doc["items"], function(i, row) {
-		if(row.delivery_note) frappe.model.clear_doc("Delivery Note", row.delivery_note)
-	})
-
-	if(cur_frm.doc.is_pos) {
-		cur_frm.msgbox = frappe.msgprint(format('<a class="btn btn-primary" \
-			onclick="cur_frm.print_preview.printit(true)" style="margin-right: 5px;">{0}</a>\
-			<a class="btn btn-default" href="javascript:new_doc(cur_frm.doctype);">{1}</a>', [
-			__('Print'), __('New')
-		]));
-
-	} else if(cint(frappe.boot.notification_settings.sales_invoice)) {
-		cur_frm.email_doc(frappe.boot.notification_settings.sales_invoice_message);
-	}
-}
-
-cur_frm.set_query("debit_to", function(doc) {
-	// filter on Account
-	if (doc.customer) {
-		return {
-			filters: {
-				'account_type': 'Receivable',
-				'is_group': 0,
-				'company': doc.company
-			}
-		}
-	} else {
-		return {
-			filters: {
-				'report_type': 'Balance Sheet',
-				'is_group': 0,
-				'company': doc.company
-			}
-		}
-	}
-});
+cur_frm.add_fetch("item_code", "stock_uom", "stock_uom");
--- drop_ship/drop_ship/doctype/drop_ship_settings/drop_ship_settings.py
+++ drop_ship/drop_ship/doctype/drop_ship_settings/drop_ship_settings.py
@@ -8,3 +8,30 @@ from frappe.model.document import Document
 
 class DropShipSettings(Document):
 	pass
+
+@frappe.whitelist()
+def get_account(company):
+	
+	account_list = []
+	
+	receivable_account = frappe.db.sql("""select account from `tabDrop Ship Settings Receivable`
+		where company = %s"""\
+		,company , as_dict=1)
+
+	if not receivable_account:
+		receivable_account = {"account":"none"}
+
+	income_account = frappe.db.sql("""select account from `tabDrop Ship Settings Income`
+		where company = %s"""\
+		,company , as_dict=1)
+
+	if not income_account:
+		income_account = {"account":"none"}
+	
+	for item in receivable_account:
+		account_list.append(item or "none")
+
+	for item in income_account:
+		account_list.append(item or "none")
+
+	return account_list
