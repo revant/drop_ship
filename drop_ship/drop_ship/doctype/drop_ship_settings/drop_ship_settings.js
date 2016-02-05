780bace665581892cda7aac08fad47d4997a7d23
Revant Nandgaonkar <revant.one@gmail.com>
2016-02-05 15:02:20 +0530

    Validation on Default Settings

---
+++ drop_ship/drop_ship/doctype/drop_ship_settings/drop_ship_settings.js
@@ -0,0 +1,35 @@
+// License: See license.txt
+
+/* Set Account Selection filter to show only Ledgers with group_or_ledger is "Ledger" */
+
+cur_frm.set_query("account", "receivable_account", function(doc, cdt, cdn) {
+   return{
+      filters: [
+         ['Account', 'is_group', '=', 0],
+      ]
+   }
+});
+
+cur_frm.set_query("account", "income_account", function(doc, cdt, cdn) {
+   return{
+      filters: [
+         ['Account', 'is_group', '=', 0],
+      ]
+   }
+});
+
+cur_frm.set_query("account", "payable_account", function(doc, cdt, cdn) {
+   return{
+      filters: [
+         ['Account', 'is_group', '=', 0],
+      ]
+   }
+});
+
+cur_frm.set_query("account", "cost_center", function(doc, cdt, cdn) {
+   return{
+      filters: [
+         ['Cost Center', 'is_group', '=', 0],
+      ]
+   }
+});
--- drop_ship/drop_ship/doctype/drop_ship_settings/drop_ship_settings.py
+++ drop_ship/drop_ship/doctype/drop_ship_settings/drop_ship_settings.py
@@ -4,10 +4,62 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class DropShipSettings(Document):
-	pass
+
+	def validate(self):
+		self.validate_accounts()
+		self.validate_repeating_companies()
+
+	def validate_repeating_companies(self):
+		ra_companies = []
+		for entry in self.receivable_account:
+			ra_companies.append(entry.company)
+
+		if len(ra_companies)!= len(set(ra_companies)):
+			frappe.throw(_("Same Company is entered more than once in Default Receivable Account"))
+
+		ia_companies = []
+		for entry in self.income_account:
+			ia_companies.append(entry.company)
+
+		if len(ia_companies)!= len(set(ia_companies)):
+			frappe.throw(_("Same Company is entered more than once in Default Income Account"))
+
+		pa_companies = []
+		for entry in self.payable_account:
+			pa_companies.append(entry.company)
+
+		if len(pa_companies)!= len(set(pa_companies)):
+			frappe.throw(_("Same Company is entered more than once in Default Payable Account"))
+
+		cc_companies = []
+		for entry in self.cost_center:
+			cc_companies.append(entry.company)
+
+		if len(cc_companies)!= len(set(cc_companies)):
+			frappe.throw(_("Same Company is entered more than once in Default Cost Center"))
+
+
+	def validate_accounts(self):
+		"""Error when Company of Ledger account doesn't match with Company Selected"""
+		for entry in self.receivable_account:
+			if frappe.db.get_value("Account", entry.account, "company") != entry.company:
+				frappe.throw(_("Account does not match with Company for Default Receivable Account"))
+
+		for entry in self.income_account:
+			if frappe.db.get_value("Account", entry.account, "company") != entry.company:
+				frappe.throw(_("Account does not match with Company for Default Outstanding Income Account"))
+
+		for entry in self.payable_account:
+			if frappe.db.get_value("Account", entry.account, "company") != entry.company:
+				frappe.throw(_("Account does not match with Company for Default Payable Account"))
+
+		for entry in self.cost_center:
+			if frappe.db.get_value("Cost Center", entry.account, "company") != entry.company:
+				frappe.throw(_("Cost Center does not match with Company"))
 
 @frappe.whitelist()
 def get_account(company):
