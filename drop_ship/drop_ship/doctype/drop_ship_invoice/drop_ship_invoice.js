949189a683c31dc3f86b140a29ddb837c3235076
Revant Nandgaonkar <revant.one@gmail.com>
2016-02-07 10:48:13 +0530

    Added Get Items from SO in DSI

--- drop_ship/drop_ship/doctype/drop_ship_invoice/drop_ship_invoice.js
+++ drop_ship/drop_ship/doctype/drop_ship_invoice/drop_ship_invoice.js
@@ -1,18 +1,22 @@
 
 // see license.txt
-
-get_items_from_so = function() {
-	frappe.model.map_current_doc({
-		method: "drop_ship.drop_ship.doctype.drop_ship_invoice.drop_ship_invoice.make_drop_ship_invoice",
-		source: cur_frm.doc.name,
-		get_query_filters: {
-			docstatus: 1,
-			status: ["not in", ["Stopped", "Closed"]],
-			per_billed: ["<", 99.99],
-			customer: cur_frm.doc.customer || undefined,
-			company: cur_frm.doc.company
-		}
-	})
-}
+frappe.ui.form.on(cur_frm.doctype, {
+refresh: function(doc) {
+		cur_frm.add_custom_button(__('Sales Order'), 
+			function() {
+			frappe.model.map_current_doc({
+				method: "drop_ship.drop_ship.doctype.drop_ship_invoice.drop_ship_invoice.make_drop_ship_invoice",
+				source_doctype: "Sales Order",
+				get_query_filters: {
+					docstatus: 1,
+					status: ["not in", ["Stopped", "Closed"]],
+					per_billed: ["<", 99.99],
+					customer: cur_frm.doc.customer || undefined,
+					company: cur_frm.doc.company
+				}
+			})
+		}, __("Get items from"));
+	},
+});
 
 cur_frm.add_fetch("item_code", "stock_uom", "stock_uom");
